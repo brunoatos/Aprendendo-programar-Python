5fa2a7151ac0c8600cc154e13e199373a68eed87
brunoatos <brunoatos1@gmail.com>
2017-07-17 01:22:22 -0300

    exerc 8

---
+++ exercicio 8 student pen test.py
@@ -0,0 +1,3 @@
+list=['maça','banana','pêra']
+print (list[0])
+'''nesse caso para imprimir uma lista de string , ela começa da esquerda para a direita com primeiro ordem 0 , tudo que está entre conchetes chama-se lista e cada string ou seja nomes com aspas .'''
