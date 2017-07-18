ece5a984e1a6f694042cf0b3e826515dbc521091
brunoatos <brunoatos1@gmail.com>
2017-07-18 15:26:18 -0300

    Update exercicio 5 student pen test.py

--- exercicio 5 student pen test.py
+++ exercicio 5 student pen test.py
@@ -1,7 +1,7 @@
-var1=int(input("Digite um numero:"))
-var2=int(input("Digite um numero:"))
+var1=15
+var2=12
 soma=(var1+var2)*2
 soma2=var1+var2*2
-print ('a soma é:',soma)
-print ('a soma é:',soma2)
+print (soma)
+print (soma2)
 '''esse fato acontece pois existe um processo de prioriodade matematico e tem que ser respeitas para ter o resultado esperado'''
