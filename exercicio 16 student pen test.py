6485de9c9427709b667fd7dae9847634722e17a6
brunoatos <brunoatos1@gmail.com>
2017-07-23 02:22:17 -0300

    Delete exercicio 16 student pen test.py

--- exercicio 16 student pen test.py
+++
@@ -1,4 +0,0 @@
-lista=['virus','notebook','desktop','S.O','Memoria']
-del lista[-1]
-print (lista)
-#exclui usando o método del , variavel e com o parametro negativo começando com o -1 ,remove inversamento os valores da lista.
