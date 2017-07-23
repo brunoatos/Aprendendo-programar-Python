3224e9553409954b0e3b828aa5d1a8d24ff0f12c
brunoatos <brunoatos1@gmail.com>
2017-07-23 00:22:50 -0500

    exercicio 16 student pent test

---
+++ exercicio 16 com função del student pen test.py
@@ -0,0 +1,6 @@
+lista=['virus','notebook','desktop','S.O','Memoria']
+print (lista)
+remover_lista=lista.pop()
+print (lista)
+print (remover_lista)
+#é simples, criei uma nova variavel e adicionei uma função pop() nela. depois printei a variavel lista depois de usar a função pop() e por ultimo printei a variavel que removi da lista.a função pop() remove o ultimo valor da lista.
