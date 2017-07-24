3bc6310d6e768c9cfa8c63a3f5ac5fa965b1b333
brunoatos <brunoatos1@gmail.com>
2017-07-24 02:26:31 -0300

    exercicio 20 student pen test.py

---
+++ exercicio 20 student pen test.py
@@ -0,0 +1,7 @@
+lista=['nomes','carros','motos','faculdade','animais','internet']
+guardada_variavel='carros'
+lista.remove(guardada_variavel)
+print ('Fui à faculdade de '+guardada_variavel)
+
+'''pois .remove() apenas remove pelo nome da lista. tem que saber realmente o nome da string para usar esta função.
+'''
