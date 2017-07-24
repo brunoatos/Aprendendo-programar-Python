b09a9c25cc8376c5307625acab5719f4b246b43c
brunoatos <brunoatos1@gmail.com>
2017-07-24 01:12:41 -0300

    exercicio 19 student pen test.py

---
+++ exercicio 19 student pen test.py
@@ -0,0 +1,3 @@
+explique Quando usar o del  e quando usar o pop() ?
+o del é quando se deseja retirar im item da lista.
+o pop é quando se deseja retirar um item de uma lista e guarda-lo em uma variavél.
