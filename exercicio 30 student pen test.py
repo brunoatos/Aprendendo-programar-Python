7d6e6fc3bf4a32c84239336dfb6de8415a39813e
brunoatos <brunoatos1@gmail.com>
2017-07-25 23:26:01 -0300

    desafio 30

--- exercicio 30 student pen test.py
+++ exercicio 30 student pen test.py
@@ -1,4 +1,3 @@
-lista_de_carros=['Honda Civic','Corolla','306','Mustang','Ferrari']
-for lista_de_carros in lista_de_carros:
-    print (lista_de_carros)
-#pois o loop for lista_de_carros até o final ou seja todos os itens dessa lista.
+mesa=['carlos','bruno','amaro']
+for mesa in mesa:
+    print (mesa)
