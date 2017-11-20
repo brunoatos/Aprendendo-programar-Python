77991f17f1b40f84ffb00fade818f15b0066e187
brunoatos <brunoatos1@gmail.com>
2017-11-19 22:18:20 -0200

    desafio30.py

---
+++ desafio30.py
@@ -0,0 +1,4 @@
+lista= ('carro','dinheiro','familia','loja','casa')
+# Esse for é para mostrar todos os elementos da lista. for é uma repetição finita ,onde tem o inicio e o fim.
+for i in range(0,len(lista)):
+    print (lista[i])
